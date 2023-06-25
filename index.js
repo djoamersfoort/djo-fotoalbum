8e976341a30d3096205d2eeb5c37bbd5978b9ac4
sverben <59171289+sverben@users.noreply.github.com>
2023-06-25 21:53:22 +0200

    Update to ES-Module + use djo-login-js lib

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-const express = require("express");
-const session = require("express-session");
-let configFile = require("./data/config.json");
-const { AuthorizationCode } = require("simple-oauth2");
-const axios = require("axios");
-const multer = require("multer");
-const fs = require("fs");
-let albums = require("./data/albums.json");
-const { exec } = require("child_process");
-const sharp = require("sharp");
-const { v4: uuid } = require("uuid");
-const bodyParser = require("body-parser");
-const ffmpeg = require("fluent-ffmpeg");
-const sizeOf = require("image-size");
+import express from 'express'
+import multer from 'multer'
+import fs from 'fs'
+import { exec } from 'child_process'
+import sharp from 'sharp'
+import { v4 as uuid } from 'uuid'
+import bodyParser from 'body-parser'
+import ffmpeg from 'fluent-ffmpeg'
+import sizeOf from 'image-size'
+import Login from '@djoamersfoort/djo-login-js'
+
+let configFile, albums
+if (fs.existsSync('./data/config.json')) {
+    configFile = JSON.parse(fs.readFileSync('./data/config.json').toString())
+} else {
+    fs.writeFileSync('./data/config.json', JSON.stringify({
+        oauth: {
+            client: {
+                id: 'CLIENT_ID',
+                secret: 'CLIENT_SECRET'
+            }
+        },
+        base_uri: 'http://localhost:3000',
+        lastVersion: '2022.8.27.1'
+    }))
+    throw 'Generated example Config, please customize settings and re-run'
+}
+if (fs.existsSync('./data/albums.json')) {
+    albums = JSON.parse(fs.readFileSync('./data/albums.json').toString())
+} else {
+    albums = []
+}
 
 if (!configFile.lastVersion) {
     console.log("Starting migration to version 2022.8.27.1")
     require("./migrate");
-    configfile = require("./data/config.json");
+    configFile = require("./data/config.json");
     albums = require("./data/albums.json");
 }
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-    secret: configFile.session_key,
-    saveUninitialized:true,
-    cookie: { maxAge: 7 * 24 * 3600 * 1000 }, // 7 Days (in milliseconds)
-    resave: false
-}))
-const client = new AuthorizationCode(configFile.oauth);
+
+const djo = new Login(configFile.oauth.client.id, configFile.oauth.client.secret, `${configFile.base_uri}/callback`, 'user/basic')
+app.use(djo.session)
+
 const multerStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "data/files");
@@ -51,76 +66,21 @@ const upload = multer({
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    if (typeof req.session.user === "undefined") {
-        const authorizationUri = client.authorizeURL({
-            redirect_uri: `${configFile.base_uri}/callback`,
-            scope: "user/basic",
-            state: req.query.state
-        });
-
-        return res.redirect(authorizationUri);
-    }
-
-    res.sendFile(`${__dirname}/public/page.html`);
-});
+app.get("/", djo.requireLogin, (req, res) => {
+    res.sendFile(`${process.cwd()}/public/page.html`);
+})
 app.get("/album", (req, res) => {
     if (typeof albums[parseInt(req.query.album)] === "undefined") return res.send("not an album!");
     if (typeof req.session.user === "undefined" && !albums[parseInt(req.query.album)].public) {
-        const authorizationUri = client.authorizeURL({
-            redirect_uri: `${configFile.base_uri}/callback`,
-            scope: "user/basic",
-            state: req.query.state
-        });
-
-        return res.redirect(authorizationUri);
+        return res.redirect(djo.authorize_uri(req.url));
     }
 
-    res.sendFile(`${__dirname}/public/album.html`);
+    res.sendFile(`${process.cwd()}/public/album.html`);
 })
-app.get("/camera", (req, res) => {
-    if (typeof req.session.user === "undefined") {
-        const authorizationUri = client.authorizeURL({
-            redirect_uri: `${configFile.base_uri}/callback`,
-            scope: "user/basic",
-            state: req.query.state
-        });
-
-        return res.redirect(authorizationUri);
-    }
-
-    res.sendFile(`${__dirname}/public/camera.html`);
+app.get("/camera", djo.requireLogin, (req, res) => {
+    res.sendFile(`${process.cwd()}/public/camera.html`);
 })
-app.get("/callback", async (req, res) => {
-    if (typeof req.query.code === "undefined") return res.send("No");
-
-    const tokenParams = {
-        code: req.query.code,
-        redirect_uri: `${configFile.base_uri}/callback`,
-        scope: "user/basic",
-    };
-
-    try {
-        const tokenDetails = await client.getToken(tokenParams);
-        const accessToken = tokenDetails.token.access_token;
-
-        axios.get("https://leden.djoamersfoort.nl/api/v1/member/details", {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        }) .then(_res => {
-            req.session.type = _res.data.accountType;
-            req.session.user = _res.data.id;
-
-            return res.redirect("/");
-        })
-            .catch(err => {
-                return res.send("An error occurred while trying to fetch user data!");
-            })
-    } catch (e) {
-        return res.send("An error occurred while handling login process!");
-    }
-})
+app.get("/callback", djo.callback)
 app.get("/logout", (req, res) => {
     delete req.session.user;
 
@@ -135,8 +95,7 @@ const getMetaData = async (file) => {
         })
     })
 }
-app.post("/upload/:album", upload.array("photo"), async (req, res) => {
-    if (typeof req.session.user === "undefined") return res.json({error: 1, msg: "Invalid user!"});
+app.post("/upload/:album", djo.requireLogin, upload.array("photo"), async (req, res) => {
     if (!req.files) return res.json({error: 1, msg: "No files were uploaded!"});
     for (const i in req.files) {
         const file = req.files[i];
@@ -208,18 +167,17 @@ app.get("/file/:album/:id", (req, res) => {
     const filter = albums[req.params.album].files.filter(file => file.id === req.params.id);
     if (filter.length === 0) return res.send("File not in album!");
 
-    res.sendFile(`${__dirname}/data/files/${req.params.id}`);
+    res.sendFile(`${process.cwd()}/data/files/${req.params.id}`);
 });
 app.get("/getAlbums", (req, res) => {
     res.json(albums.sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
 })
 
-app.post("/createAlbum", (req, res) => {
-    if (typeof req.session.user === "undefined") return res.send("Not signed in!");
+app.post("/createAlbum", djo.requireLogin, (req, res) => {
     if (typeof req.body.name === "undefined") return res.send("No name given!");
     if (typeof req.body.public === "undefined") return res.send("No public given!");
     if (typeof req.body.description === "undefined") return res.send("No description given!");
-    if (!req.session.type.split(",").includes("begeleider")) return res.send("Not authorized!");
+    if (!req.session.djo.accountType.split(",").includes("begeleider")) return res.send("Not authorized!");
 
     albums.push({
         name: req.body.name,
@@ -232,12 +190,11 @@ app.post("/createAlbum", (req, res) => {
 
     res.redirect("/");
 })
-app.get("/delete/:album/:id", (req, res) => {
-    if (typeof req.session.user === "undefined") return res.send("Not signed in!");
+app.get("/delete/:album/:id", djo.requireLogin, (req, res) => {
     if (typeof albums[req.params.album] === "undefined") return res.send("Album not found!");
     const filter = albums[req.params.album].files.filter(file => file.id === req.params.id);
     if (filter.length === 0) return res.send("File not in album!");
-    if (filter[0].user !== req.session.user && !req.session.type.split(",").includes("begeleider")) return res.send("Incorrect permissions!");
+    if (filter[0].user !== req.session.user && !req.session.djo.accountType.split(",").includes("begeleider")) return res.send("Incorrect permissions!");
 
     albums[req.params.album].files.splice(albums[req.params.album].files.indexOf(filter[0]), 1);
     fs.unlink(`./data/files/${req.params.id}`, () => {
@@ -245,8 +202,7 @@ app.get("/delete/:album/:id", (req, res) => {
     });
     fs.writeFileSync("data/albums.json", JSON.stringify(albums));
 })
-app.post("/bulkDelete", (req, res) => {
-    if (typeof req.session.user === "undefined") return res.send("Not signed in!");
+app.post("/bulkDelete", djo.requireLogin, (req, res) => {
     if (typeof req.body.files === "undefined") return res.send("No files given!");
     if (!Array.isArray(req.body.files)) return res.send("Files must be an array!");
 
@@ -256,7 +212,7 @@ app.post("/bulkDelete", (req, res) => {
         if (typeof albums[req.body.album] === "undefined") continue;
         const filter = albums[req.body.album].files.filter(a => a.id === file);
         if (filter.length === 0) continue;
-        if (filter[0].user !== req.session.user && !req.session.type.split(",").includes("begeleider")) continue;
+        if (filter[0].user !== req.session.user && !req.session.djo.accountType.split(",").includes("begeleider")) continue;
 
         albums[req.body.album].files.splice(albums[req.body.album].files.indexOf(filter[0]), 1);
         fs.unlink(`./data/files/${file}`, () => {});
@@ -266,22 +222,19 @@ app.post("/bulkDelete", (req, res) => {
     res.json({error: 0});
 })
 
-app.get("/setPreview/:album/:file", (req, res) => {
-    if (typeof req.session.user === "undefined") return res.send("Not signed in!");
+app.get("/setPreview/:album/:file", djo.requireLogin, (req, res) => {
     if (typeof albums[req.params.album] === "undefined") return res.send("Album not found!");
     if (!fs.existsSync(`data/files/${req.params.file}`)) return res.send("File not found!");
-    if (!req.session.type.split(",").includes("begeleider")) return res.send("You can't set this file as preview!");
+    if (!req.session.djo.accountType.split(",").includes("begeleider")) return res.send("You can't set this file as preview!");
 
     albums[req.params.album].preview = req.params.file;
-    fs.writeFileSync(`${__dirname}/data/albums.json`, JSON.stringify(albums));
+    fs.writeFileSync(`${process.cwd()}/data/albums.json`, JSON.stringify(albums));
 
     res.redirect(`/album?album=${req.params.album}`);
 });
-app.get("/permissions", (req, res) => {
-    if (typeof req.session.user === "undefined") return res.send("Not signed in!");
-
+app.get("/permissions", djo.requireLogin, (req, res) => {
     return res.json([
-        req.session.type.split(",").includes("begeleider") ? "" : req.session.user,
+        req.session.djo.accountType.split(",").includes("begeleider") ? "" : req.session.user,
     ])
 })
 
